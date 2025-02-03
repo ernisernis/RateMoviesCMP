96500f78d3ed6e5a151cfebe48afb3010f4408bf
ernisernis <ernestas.dainys@gmail.com>
2025-02-03 17:35:40 +0200

    Implement status bar padding to MovieDetailScreen image item action buttons

--- composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/movie/presentation/movie_detail/MovieDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/movie/presentation/movie_detail/MovieDetailScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -95,6 +96,7 @@ fun MovieDetailScreen(
             // Close, Bookmark buttons
             Row(
                 modifier = Modifier
+                    .statusBarsPadding()
                     .fillMaxWidth()
                     .padding(Dimens.MovieDetailItemPaddingBig),
                 horizontalArrangement = Arrangement.SpaceBetween
