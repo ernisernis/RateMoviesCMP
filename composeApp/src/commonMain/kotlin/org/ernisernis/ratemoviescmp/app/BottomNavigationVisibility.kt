6fa93de5b45ba61927ebced3d311d1350565cd0d
ernisernis <ernestas.dainys@gmail.com>
2025-02-03 16:29:06 +0200

    Fix BottomNavigationVisibility background color

--- composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/app/BottomNavigationVisibility.kt
+++ composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/app/BottomNavigationVisibility.kt
@@ -4,7 +4,10 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 
 @Composable
@@ -13,6 +16,8 @@ fun BottomNavigationVisibility(
     content: @Composable AnimatedVisibilityScope.() -> Unit
 ) {
    AnimatedVisibility(
+       modifier = Modifier
+           .background(MaterialTheme.colorScheme.surface),
        visible = visible,
        enter = slideInVertically(initialOffsetY = { it }),
        exit = slideOutVertically(targetOffsetY = { it }),
