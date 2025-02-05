5529893d6df60a426ae6cc19eb6c9b88353b631d
ernisernis <ernestas.dainys@gmail.com>
2025-02-05 11:59:53 +0200

    Add negative number cases to format vote average test

--- composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/core/presentation/NumberExtensions.kt
+++ composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/core/presentation/NumberExtensions.kt
@@ -2,7 +2,9 @@ package org.ernisernis.ratemoviescmp.core.presentation
 
 import kotlin.math.round
 
+// String should always be between 0 and 10
 internal fun Double.formatVoteAverage(): String {
-    val rounded = round(10.0 * this) / 10.0
+    val safeDouble = this.coerceIn(0.0, 10.0)
+    val rounded = round(10.0 * safeDouble) / 10.0
     return if (rounded % 1.0 == 0.0) rounded.toInt().toString() else rounded.toString()
 }
--- composeApp/src/commonTest/kotlin/FormatVoteAverageKtTest.kt
+++ composeApp/src/commonTest/kotlin/FormatVoteAverageKtTest.kt
@@ -2,7 +2,6 @@ import org.ernisernis.ratemoviescmp.core.presentation.formatVoteAverage
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-// The value from the backend is always between 0.0 and 10.0
 class FormatVoteAverageKtTest {
 
     private val testCases = listOf(
@@ -13,6 +12,10 @@ class FormatVoteAverageKtTest {
         Pair(7.65, "7.6"),
         Pair(7.60, "7.6"),
         Pair(5.132560, "5.1"),
+        Pair(-7.60, "0"),
+        Pair(-3.6, "0"),
+        Pair(-1.0, "0"),
+        Pair(11.0, "10"),
     )
 
     @Test
