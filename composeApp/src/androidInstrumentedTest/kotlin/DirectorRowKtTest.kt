7128d363e03e2f5fd8423e9cd6271dbdca4cd451
ernisernis <ernestas.dainys@gmail.com>
2025-02-05 19:45:53 +0200

    Implement DirectorRow composable test, remove unnecessary test

---
+++ composeApp/src/androidInstrumentedTest/kotlin/DirectorRowKtTest.kt
@@ -0,0 +1,40 @@
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.ernisernis.ratemoviescmp.movie.presentation.movie_detail.components.DirectorRow
+import org.junit.Rule
+import org.junit.Test
+
+class DirectorRowKtTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun director_row_content_displays_correctly() {
+        val text1 = "Sam"
+        val text2 = "Akinawa"
+
+        composeTestRule.setContent {
+            DirectorRow(
+                director = text1,
+                writer = text2,
+            )
+        }
+
+        composeTestRule.onNodeWithText(text1, substring = true).assertExists()
+        composeTestRule.onNodeWithText(text2, substring = true).assertExists()
+    }
+
+    @Test
+    fun director_row_content_displays_correctly_2() {
+        val text1 = null
+        val text2 = "Akinawa"
+
+        composeTestRule.setContent {
+            DirectorRow(
+                director = text1,
+                writer = text2,
+            )
+        }
+    }
+}
