ad664ebacc3c0262c8e141a1c15487c10d42d279
ernisernis <ernestas.dainys@gmail.com>
2025-02-05 11:14:00 +0200

    Implement 'parameterized' test workaround.

--- composeApp/src/commonTest/kotlin/FormatVoteAverageKtTest.kt
+++ composeApp/src/commonTest/kotlin/FormatVoteAverageKtTest.kt
@@ -5,63 +5,21 @@ import kotlin.test.assertEquals
 // The value from the backend is always between 0.0 and 10.0
 class FormatVoteAverageKtTest {
 
-    @Test
-    fun `Case 1`() {
-        val value = 9.11
-        val expected = "9.1"
-
-        val actual = value.formatVoteAverage()
-
-        assertEquals(expected, actual, "String should be $expected")
-    }
-
-    @Test
-    fun `Case 2`() {
-        val value = 8.0
-        val expected = "8"
-
-        val actual = value.formatVoteAverage()
-
-        assertEquals(expected, actual, "String should be $expected")
-    }
+    private val testCases = listOf(
+        Pair(9.11, "9.1"),
+        Pair(8.0, "8"),
+        Pair(7.00, "7"),
+        Pair(7.67, "7.7"),
+        Pair(7.65, "7.6"),
+        Pair(7.60, "7.6"),
+        Pair(5.132560, "5.1"),
+    )
 
     @Test
-    fun `Case 3`() {
-        val value = 7.00
-        val expected = "7"
-
-        val actual = value.formatVoteAverage()
-
-        assertEquals(expected, actual, "String should be $expected")
-    }
-
-    @Test
-    fun `Case 4`() {
-        val value = 7.67
-        val expected = "7.7"
-
-        val actual = value.formatVoteAverage()
-
-        assertEquals(expected, actual, "String should be $expected")
-    }
-
-    @Test
-    fun `Case 5`() {
-        val value = 7.65
-        val expected = "7.6"
-
-        val actual = value.formatVoteAverage()
-
-        assertEquals(expected, actual, "String should be $expected")
-    }
-
-    @Test
-    fun `Case 6`() {
-        val value = 7.60
-        val expected = "7.6"
-
-        val actual = value.formatVoteAverage()
-
-        assertEquals(expected, actual, "String should be $expected")
+    fun `Check if formatter outputs correct values`() {
+        testCases.forEach { (value, expected) ->
+            val actual = value.formatVoteAverage()
+            assertEquals(expected, actual, "String should be $expected")
+        }
     }
 }
