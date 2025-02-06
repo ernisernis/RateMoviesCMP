28b9515c468a42a5833e0e288c479dedb72b67f8
ernisernis <ernestas.dainys@gmail.com>
2025-02-06 20:41:57 +0200

    Add missing rating entity class

---
+++ composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/movie/data/database/entities/RatingEntity.kt
@@ -0,0 +1,16 @@
+package org.ernisernis.ratemoviescmp.movie.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class RatingEntity(
+    @PrimaryKey(autoGenerate = false) val id: Int,
+    val posterPath: String,
+    val title: String,
+    val releaseDate: String,
+    val runtimeFormatted: String?,
+    val voteAverage: String,
+    val description: String?,
+    val userRating: Int,
+)
