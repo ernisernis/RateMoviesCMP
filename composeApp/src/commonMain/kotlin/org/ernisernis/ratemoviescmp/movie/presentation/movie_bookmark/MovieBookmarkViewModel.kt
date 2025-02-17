2e5e540f04620bc7bebb1a14cf177e9a4559566a
ernisernis <ernestas.dainys@gmail.com>
2025-02-17 15:42:56 +0200

    Change tab viewmodel initializations

--- composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/movie/presentation/movie_bookmark/MovieBookmarkViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/movie/presentation/movie_bookmark/MovieBookmarkViewModel.kt
@@ -3,22 +3,28 @@ package org.ernisernis.ratemoviescmp.movie.presentation.movie_bookmark
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.ernisernis.ratemoviescmp.core.presentation.OnetimeWhileSubscribed
 import org.ernisernis.ratemoviescmp.movie.domain.MovieRepository
 
 class MovieBookmarkViewModel(
     private val movieRepository: MovieRepository,
 ) : ViewModel() {
     private val _state = MutableStateFlow(MovieBookmarkState())
-    val state = _state.asStateFlow()
-
-    init {
-        observeBookmarks()
-    }
+    val state = _state
+        .onStart {
+            observeBookmarks()
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = OnetimeWhileSubscribed(5_000),
+            initialValue = MovieBookmarkState()
+        )
 
     fun onAction(action: MovieBookmarkAction) {
         when (action) {
--- composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/movie/presentation/movie_profile/MovieProfileViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/ernisernis/ratemoviescmp/movie/presentation/movie_profile/MovieProfileViewModel.kt
@@ -3,11 +3,13 @@ package org.ernisernis.ratemoviescmp.movie.presentation.movie_profile
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.ernisernis.ratemoviescmp.core.presentation.OnetimeWhileSubscribed
 import org.ernisernis.ratemoviescmp.movie.data.mappers.toRatingUi
 import org.ernisernis.ratemoviescmp.movie.domain.ProfileRepository
 
@@ -16,11 +18,15 @@ class MovieProfileViewModel(
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(MovieProfileState())
-    val state = _state.asStateFlow()
-
-    init {
-        observeRatings()
-    }
+    val state = _state
+        .onStart {
+            observeRatings()
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = OnetimeWhileSubscribed(5_000),
+            initialValue = MovieProfileState()
+        )
 
     private fun observeRatings() {
         profileRepository
